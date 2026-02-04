ad803fe75719c35d13a7f1597706c725b36cd878
Developer LL <new.jasjan.programmer@gmail.com>
2026-02-04 22:02:28 +0500

    chore: some docker and deployment

--- next.config.js
+++ next.config.js
@@ -3,26 +3,27 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    formats: ['image/avif', 'image/webp'],
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       // Local development
-      { protocol: 'http', hostname: '127.0.0.1' },
-      { protocol: 'http', hostname: 'localhost' },
+      { protocol: "http", hostname: "127.0.0.1" },
+      { protocol: "http", hostname: "localhost" },
 
       // 🔥 Amediatv uchun
-      { protocol: 'http', hostname: 'cdn.amediatv.uz' },
-      { protocol: 'https', hostname: 'cdn.amediatv.uz' },
+      { protocol: "http", hostname: "cdn.amediatv.uz" },
+      { protocol: "https", hostname: "cdn.amediatv.uz" },
 
       // Boshqa rasm manbalari
-      { protocol: 'https', hostname: 'i.pinimg.com' },
-      { protocol: 'https', hostname: 'i.namu.wiki' },
-      { protocol: 'https', hostname: 'media.tenor.com' },
-      { protocol: 'https', hostname: 'resizing.flixster.com' },
+      { protocol: "https", hostname: "i.pinimg.com" },
+      { protocol: "https", hostname: "i.namu.wiki" },
+      { protocol: "https", hostname: "media.tenor.com" },
+      { protocol: "https", hostname: "resizing.flixster.com" },
 
       // 🆕 Amazon rasmlari uchun
-      { protocol: 'https', hostname: 'm.media-amazon.com' },
+      { protocol: "https", hostname: "m.media-amazon.com" },
     ],
   },
+  output: "standalone",
 };
 
 export default nextConfig;
